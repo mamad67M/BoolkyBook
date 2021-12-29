95eae940d17ed242fe87a33ef9b46180e00451fd
DIALLO <mam.diallo163@gmail.com>
2021-12-29 23:05:19 +0100

    ajout controller + vues

---
+++ BoolkyBook_app_web/Controllers/CategorieController.cs
@@ -0,0 +1,45 @@
+﻿using BoolkyBook_app_web.Data;
+using BoolkyBook_app_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoolkyBook_app_web.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategorieController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> list = _db.Categories;
+            return View(list);
+        }
+
+        // Get
+        [HttpGet]
+        public IActionResult Create()
+        {  
+            return View();
+        }
+        // post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category c)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(c);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            return View(c);
+        }
+    }
+}
--- BoolkyBook_app_web/Models/Category.cs
+++ BoolkyBook_app_web/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,8 +12,11 @@ namespace BoolkyBook_app_web.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Nom")]
         public string Name { get; set; }
+        [DisplayName("Numéro de commande")]
         public int DisplayOrder { get; set; }
+        [DisplayName("Date de Creation")]
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
