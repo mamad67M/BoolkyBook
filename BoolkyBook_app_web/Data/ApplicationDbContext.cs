54a0c588fc090601021df35d3f060e4f86bb572b
DIALLO <mam.diallo163@gmail.com>
2021-12-29 14:16:02 +0100

    first commit

---
+++ BoolkyBook_app_web/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BoolkyBook_app_web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoolkyBook_app_web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
