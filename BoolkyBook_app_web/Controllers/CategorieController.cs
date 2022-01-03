42bf42ec16ea3933902d3aea78351183432e2498
DIALLO <mam.diallo163@gmail.com>
2022-01-03 18:41:43 +0100

    traitement_edit_delete_create_deployement

--- BoolkyBook_app_web/Controllers/CategorieController.cs
+++ BoolkyBook_app_web/Controllers/CategorieController.cs
@@ -36,10 +36,80 @@ namespace BoolkyBook_app_web.Controllers
             {
                 _db.Categories.Add(c);
                 _db.SaveChanges();
+                TempData["success"] = "Catégorie créée avec succès";
                 return RedirectToAction("Index");
             }
            
             return View(c);
+        }
+        // Get Edit
+        [HttpGet]
+        public IActionResult Edit(int ? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Post 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category c)
+        {    
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(c);
+                _db.SaveChanges();
+                TempData["success"] = "Catégorie modifiée avec succès";
+
+                return RedirectToAction("Index");
+            }
+            return View(c);
+        }
+
+        // Get Edit
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Post 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int ? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if ( obj == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Catégorie supprimée avec succès";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
